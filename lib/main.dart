90bd31dc92c0b654be620ae9c2be2d5709b4ceed
Cabavas <cabavas@gmail.com>
2023-01-20 12:03:50 -0300

    Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_example/provider/provider_page.dart';
+import 'package:provider_example/provider/user_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,15 +9,41 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Provider(
+      create: (_) {
+        return UserModel(
+            name: 'Caio Vasconcelos',
+            imgAvatar:
+                'https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg',
+            birthDate: '13/03/1989');
+      },
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {'/provider': (_) => const ProviderPage()},
+        home: Builder(
+          builder: (context) {
+            return Scaffold(
+                body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/Provider');
+                    },
+                    child: const Text('Provider')),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Change Notifier')),
+              ],
+            ));
+          },
+        ),
       ),
-      home: const Container(),
     );
   }
 }
---
+++ lib/provider/provider_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_example/provider/user_model.dart';
+
+class ProviderPage extends StatelessWidget {
+  const ProviderPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var user = Provider.of<UserModel>(context);
+    //var user = context.read<UserModel>();
+    //var imgAvatar = context.select<UserModel, String>((userModel) => userModel.imgAvatar);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.imgAvatar),
+                radius: 60,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(user.name),
+                  Text(' (${user.birthDate})'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
